500b364bbac7b477bd7fcaa5a9561b6b280d1326
Jositote <ja.sebasdavila@gmail.com>
2022-07-08 16:50:44 +0200

    Actualizado prueba_gets

--- prueba_gets.py
+++ prueba_gets.py
@@ -1,3 +1,29 @@
 from connection_utils.communication_controllers import can_utils
+import time
 
 can_utils.CAN.get_rpm_can()
+time.sleep(1)
+
+can_utils.CAN.get_speed_FL_can()
+time.sleep(1)
+
+can_utils.CAN.get_speed_FR_can()
+time.sleep(1)
+
+can_utils.CAN.get_amr()
+time.sleep(1)
+
+can_utils.CAN.get_ASState()
+time.sleep(1)
+
+can_utils.CAN.get_brake_pressure()
+time.sleep(1)
+
+can_utils.CAN.get_clutch_state()
+time.sleep(1)
+
+can_utils.CAN.get_steer_angle()
+time.sleep(1)
+
+can_utils.CAN.get_throttle_pos()
+time.sleep(1)
