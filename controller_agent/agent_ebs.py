bc8bec92f57aa3917c1534eacc73f8f6b5b9ae34
Jositote <ja.sebasdavila@gmail.com>
2022-07-27 13:55:10 +0200

    Creado agente de curva 180º

--- controller_agent/agent_ebs.py
+++ controller_agent/agent_ebs.py
@@ -1,6 +1,12 @@
 from controller_agent.agent import AgentAccelerationYoloFast
-
+from trayectory_estimation.cone_processing import ConeProcessingTest180
 
 class AgentEBS(AgentAccelerationYoloFast):
     def __init__(self, logger, target_speed=45.):
         super().__init__(logger=logger, target_speed=45.)
+
+class AgentTest180(AgentAccelerationYoloFast):
+    def __init__(self, logger, target_speed=10.):
+        super().__init__(logger=logger, target_speed=target_speed)
+        self.cone_processing = ConeProcessingTest180()
+
