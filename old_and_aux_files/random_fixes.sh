2c8865e4b87e054a60888cadd0170db68ccdf744
rubenayla <ruben.jimenezmejias@gmail.com>
2023-05-02 13:21:55 +0200

    bug didn't work with image because of simulator client
    Update README.md
    
    Update README.md
    
    Update README.md
    
    .
    
    works with ZED, but without threads
    
    works with zed like normal webcam
    
    Update README.md
    
    Update requirements_computer.txt
    
    simplifying agent
    
    agent simplified
    
    warp commented

---
+++ old_and_aux_files/random_fixes.sh
@@ -0,0 +1,7 @@
+
+
+
+# To fix tornado dependency for fsds simulator:
+pip install tornado==4.5.3
+pip3 install msgpack-rpc-python
+# The packages ipykernel 6.22.0, jupyter-client 8.2.0, jupyter-server 2.5.0, nbclassic 0.5.6, notebook 6.5.4, terminado 0.17.1 won't work because of having the old tornado dependency
